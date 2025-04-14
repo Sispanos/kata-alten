6964fe65a3cd95f5e2c8d802bbd7d828e8d636ef
Sispanos <jamalbouhssine@gmail.com>
2025-04-14 17:40:50 +0200

    :art: create entities : product & cart & wishlist

---
+++ src/main/java/com/product/manage/model/Wishlist.java
@@ -0,0 +1,26 @@
+package com.product.manage.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
